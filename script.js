92f3becafb1c50eb668a8fa7beb573471a0cbd4a
marianinha1 <108131541+marianinha1@users.noreply.github.com>
2024-08-22 11:51:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { aleatorio } from "./aleatorio.js";
+import { aleatorio, nome } from "./aleatorio.js";
 import { perguntas } from "./perguntas.js";
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
@@ -40,7 +40,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar"); botaoJogarNovamente.addEventListener("click", jogaNovamente());
@@ -57,5 +57,10 @@ function aleatorio(lista) {
         caixaResultado.classList.remove("mostrar");
         mostraPergunta();
     }
+    function substituiNome(){
+        for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+            }
+        }
 mostraPergunta();
 
