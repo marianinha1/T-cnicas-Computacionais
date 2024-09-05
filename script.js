337589041367c8ff2e3279451616dc9fea663bd4
marianinha1 <108131541+marianinha1@users.noreply.github.com>
2024-09-05 11:01:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
 
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+function iniciaJogo() {
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
+    substituiNome();
+    mostraPergunta();
+    }
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
